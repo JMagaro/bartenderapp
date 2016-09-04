e3cb94c0aef24ac0032f5fc6525b803a0d086126
Jared Magaro <brandonmagaro@gmail.com>
2016-09-04 07:30:50 -0700

    initial commit

---
+++ BartenderApp/Models/Drink.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BartenderApp.Models
+{
+    public class Drink
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class DrinkDbContext : DbContext
+    {
+        public DbSet <Drink> Drinks { get; set; }
+    }
+}
---
+++ BartenderApp/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BartenderApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public Drink Drink { get; set; }
+    }
+    public class OrderDbContext : DbContext
+    {
+        public DbSet <Order> Order { get; set; }
+    }
+}
